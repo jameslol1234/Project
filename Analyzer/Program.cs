e632fe7fddb66ce97c29b3f132f753a8eadf3a51
Junih <kowart995@gmail.com>
2019-10-15 18:40:08 +0200

    Change in program punctuation counting function

--- Analyzer/Program.cs
+++ Analyzer/Program.cs
@@ -42,7 +42,7 @@ namespace Projekt
                 }
                 catch (FileNotFoundException)
                 {
-                    Console.WriteLine("Could not find file " +FileName);
+                    Console.WriteLine("Could not find file " + FileName);
                 }
 
             }
@@ -111,7 +111,7 @@ namespace Projekt
             int PuncMarksQty = 0;
             foreach (char c in FileText)
             {
-                if (char.IsPunctuation(c))
+                if (c == '?' || c == '.')
                 {
                     PuncMarksQty++;
                 }
